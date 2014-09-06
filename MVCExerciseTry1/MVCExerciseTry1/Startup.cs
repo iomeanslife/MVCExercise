d70b9c9bedba2406a29e104df7b2cc7c2816d898
Salem5 <salem5@outlook.com>
2014-09-06 17:31:34 +0100

    initial

---
+++ MVCExerciseTry1/MVCExerciseTry1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCExerciseTry1.Startup))]
+namespace MVCExerciseTry1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
