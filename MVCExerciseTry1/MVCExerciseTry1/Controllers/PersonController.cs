87144214e5cb4c5ba3eda3be8adc9acee32730e8
Salem5 <salem5@outlook.com>
2014-09-09 15:03:17 +0100

    Hardcoding personal details

---
+++ MVCExerciseTry1/MVCExerciseTry1/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using MVCExerciseTry1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExerciseTry1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            Character model = new Character()
+            {
+                FirstName = "Omar",
+                SurName = "Ajerray",
+                Birthyear = DateTime.Parse("1985-06-02")
+            };
+
+            return View(model);
+        }
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        } 
+    }
+}
---
+++ MVCExerciseTry1/MVCExerciseTry1/Models/PersonViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCExerciseTry1.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public int SkillsId { set; get; }
+        public String FirstName { set; get; }
+        public String SurName { set; get; }
+        public DateTime Birthyear { set; get; }
+        public String Interessts { set; get; }
+        public String Aspirations { set; get; }
+        public float Height { set; get; }
+        public float Weight { set; get; }
+    }
+
+    public class Skill
+    {
+        public int ID { get; set; }
+        public String Title { set; get; }
+    }
+
+    public class PersonDbContext : DbContext
+    {
+        public Character Character { set; get; }
+        //public DbSet<MVCExerciseTry1.Models.Skill> Skills {get;set;}
+
+    }
+}
